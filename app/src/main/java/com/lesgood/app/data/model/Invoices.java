e2e9c71fc38006f8479586081f5b91722aa62d3d
BambangHeriSetiawan <setiawan.heri.bambang@gmail.com>
2017-12-20 13:08:56 +0700

    add invoice model

---
+++ app/src/main/java/com/lesgood/app/data/model/Invoices.java
@@ -0,0 +1,103 @@
+package com.lesgood.app.data.model;
+
+public class Invoices{
+	private int amount;
+	private int tarif;
+	private int total;
+	private String code;
+	private String iid;
+	private int totalSiswa;
+	private int fee;
+	private int totalPertemuan;
+	private int discount;
+	private String oid;
+	private String status;
+
+	public void setAmount(int amount){
+		this.amount = amount;
+	}
+
+	public int getAmount(){
+		return amount;
+	}
+
+	public void setTarif(int tarif){
+		this.tarif = tarif;
+	}
+
+	public int getTarif(){
+		return tarif;
+	}
+
+	public void setTotal(int total){
+		this.total = total;
+	}
+
+	public int getTotal(){
+		return total;
+	}
+
+	public void setCode(String code){
+		this.code = code;
+	}
+
+	public String getCode(){
+		return code;
+	}
+
+	public void setIid(String iid){
+		this.iid = iid;
+	}
+
+	public String getIid(){
+		return iid;
+	}
+
+	public void setTotalSiswa(int totalSiswa){
+		this.totalSiswa = totalSiswa;
+	}
+
+	public int getTotalSiswa(){
+		return totalSiswa;
+	}
+
+	public void setFee(int fee){
+		this.fee = fee;
+	}
+
+	public int getFee(){
+		return fee;
+	}
+
+	public void setTotalPertemuan(int totalPertemuan){
+		this.totalPertemuan = totalPertemuan;
+	}
+
+	public int getTotalPertemuan(){
+		return totalPertemuan;
+	}
+
+	public void setDiscount(int discount){
+		this.discount = discount;
+	}
+
+	public int getDiscount(){
+		return discount;
+	}
+
+	public void setOid(String oid){
+		this.oid = oid;
+	}
+
+	public String getOid(){
+		return oid;
+	}
+
+	public void setStatus(String status){
+		this.status = status;
+	}
+
+	public String getStatus(){
+		return status;
+	}
+}
